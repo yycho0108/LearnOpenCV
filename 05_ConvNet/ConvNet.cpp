6ea1e51b1b7d64f391516dadb1c99720676b6639
yycho0108 <jchocholate@gmail.com>
2016-03-18 12:34:57 -0400

    Verified that DenseLayer is capable of learning

--- 05_ConvNet/ConvNet.cpp
+++ 05_ConvNet/ConvNet.cpp
@@ -3,6 +3,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <functional>
 #include <iostream>
+#include <ctime>
 
 using namespace cv;
 using namespace std;
@@ -39,18 +40,31 @@ void sigmoid(Mat& src, Mat& dst){
 	if(&dst != &src){
 		src.copyTo(dst);
 	}
-	ForEach(dst.data,[](float x){return sigmoid(x);});
+	parallel_for_(Range(0,dst.rows*dst.cols),ForEach(dst.data,[](float a){return sigmoid(a);}));
+
 }
 
 void sigmoidPrime(Mat& src, Mat& dst){
 	if(&dst != &src){
 		src.copyTo(dst);
 	}
-	ForEach(dst.data,[](float x){return sigmoidPrime(x);});
+	parallel_for_(Range(0,dst.rows*dst.cols),ForEach(dst.data,[](float a){return sigmoidPrime(a);}));
+}
+
+void SoftMax(Mat& src, Mat& dst){
+	if(&dst != &src){
+		src.copyTo(dst);
+	}
+	exp(src,dst);
+	auto s = cv::sum(dst);
+	cv::divide(dst,s,dst);
 }
 
 void convolve(cv::InputArray I, cv::OutputArray O,cv::InputArray W){
+	//cv::filter2D(I,O,-1,W,Point(-1,-1),0.0,BORDER_CONSTANT);
+	//cout << "CONVOLVE : " <<  I.size() << endl <<  O.size() << endl;
 	return cv::filter2D(I,O,-1,W,Point(-1,-1),0.0,BORDER_CONSTANT);//change depth later
+	//currently same-size
 	//currently zero-padding
 }
 Mat ave_pool(Mat& m, Size s){
@@ -64,9 +78,14 @@ Mat ave_pool(Mat& m, Size s){
 
 class Layer{
 	public:
+		virtual ~Layer(){};
+
 		virtual std::vector<Mat>& FF(std::vector<Mat>)=0;
 		virtual std::vector<Mat>& BP(std::vector<Mat>)=0;
+		
 		virtual void update(){};
+		virtual void setup(Size){};
+		virtual Size outputSize()=0;
 };
 /* ** Dense Layer ** */
 
@@ -83,14 +102,21 @@ private:
 	std::vector<Mat> G; //maybe not necessary? idk...
 	//
 public:
-	DenseLayer(int d, int i, int o);
+	DenseLayer(int d, int s_o);
+
 	virtual std::vector<Mat>& FF(std::vector<Mat> I);
 	virtual std::vector<Mat>& BP(std::vector<Mat> G);
+	virtual void setup(Size s);
+	virtual Size outputSize();
 	virtual void update();
 };
 
-DenseLayer::DenseLayer(int d,int s_i,int s_o)
-	:d(d),s_i(s_i),s_o(s_o),W(d),b(d),dW(d),db(d),I(d),O(d),G(d){
+DenseLayer::DenseLayer(int d, int s_o)
+	:d(d),s_o(s_o),W(d),b(d),dW(d),db(d),I(d),O(d),G(d){
+}
+void DenseLayer::setup(Size s){
+	this->s_i = s.height;
+
 	for(int i=0;i<d;++i){
 		W[i] = Mat::zeros(s_o,s_i,DataType<float>::type);
 		b[i] = Mat::zeros(Size(1,s_o),DataType<float>::type);
@@ -98,6 +124,7 @@ DenseLayer::DenseLayer(int d,int s_i,int s_o)
 		db[i] = Mat::zeros(s_o,s_i,DataType<float>::type);
 		cv::randn(W[i],cv::Scalar::all(0),cv::Scalar::all(0.1));
 	}
+
 }
 
 std::vector<Mat>& DenseLayer::FF(std::vector<Mat> _I){
@@ -121,27 +148,34 @@ void DenseLayer::update(){
 		b[i] += db[i];
 	}	
 }
+Size DenseLayer::outputSize(){
+	return Size(1,s_o); //Size(width,height);
+}
 
 class FlattenLayer : public Layer{
 private:
+	int d;
+	Size s;
 	std::vector<Mat> I;
 	std::vector<Mat> O;
 	std::vector<Mat> G;
-	Size s;
 public:
-	FlattenLayer();
+	FlattenLayer(int d);
+
 	virtual std::vector<Mat>& FF(std::vector<Mat> I);
 	virtual std::vector<Mat>& BP(std::vector<Mat> G);
+	virtual void setup(Size);
+	virtual Size outputSize();
 };
 
-FlattenLayer::FlattenLayer(){
+FlattenLayer::FlattenLayer(int d):d(d){
 	O.push_back(Mat());
 }
 
 std::vector<Mat>& FlattenLayer::FF(std::vector<Mat> _I){
 	I.swap(_I);
 	int n = I.size();
-	s = I[0].size();
+	s = I[0].size(); //will be unnecessary soon
 	O[0] = I[0].reshape(0,s.width*s.height);
 	for(int i=1;i<n;++i){
 		cv::vconcat(O[0],I[i].reshape(0,s.width*s.height),O[0]);
@@ -161,17 +195,28 @@ std::vector<Mat>& FlattenLayer::BP(std::vector<Mat> _G){
 	return G;
 }
 
+void FlattenLayer::setup(Size s){
+	this->s = s;
+}
+Size FlattenLayer::outputSize(){
+	return Size(1,d*s.width*s.height);
+}
 /* ** Activation Layer ** */
 class ActivationLayer : public Layer{
 private:
 	int d;
+	Size s;
 	std::vector<Mat> I;
 	std::vector<Mat> O;
 	std::vector<Mat> G; //maybe not necessary? idk...
 public:
 	ActivationLayer();
+
 	virtual std::vector<Mat>& FF(std::vector<Mat> I);
 	virtual std::vector<Mat>& BP(std::vector<Mat> G);
+	virtual void setup(Size s);
+	virtual Size outputSize();
+	//no need to update since to trainable parameter
 };
 
 ActivationLayer::ActivationLayer(){
@@ -198,6 +243,12 @@ std::vector<Mat>& ActivationLayer::BP(std::vector<Mat> _G){
 	G.swap(_G);
 	return G;
 }
+void ActivationLayer::setup(Size s){
+	this->s = s; //doesn't really matter, but necessary to transfer size to next layer
+}
+Size ActivationLayer::outputSize(){
+	return s;
+}
 
 /* ** Convolution Layer ** */
 
@@ -216,14 +267,18 @@ private:
 	std::vector<Mat> G;
 
 public:
-	ConvLayer(int,int,Size);
+	ConvLayer(int,int);
 	~ConvLayer();
+
 	virtual std::vector<Mat>& FF(std::vector<Mat> I);
 	virtual std::vector<Mat>& BP(std::vector<Mat> G);
-	void update();
+	virtual void update();
+
+	virtual Size outputSize();
+	virtual void setup(Size);
 };
-ConvLayer::ConvLayer(int d_i, int d_o, Size s)
-	:s(s),d_i(d_i),d_o(d_o),dW(d_o){
+ConvLayer::ConvLayer(int d_i, int d_o)
+	:d_i(d_i),d_o(d_o),dW(d_o){
 	// Size Before SubSampling
 	// d_i = depth of input layers
 	// d_o = depth of output layers
@@ -236,12 +291,8 @@ ConvLayer::ConvLayer(int d_i, int d_o, Size s)
 	}
 
 	for(int o=0;o<d_o;++o){
-
 		W.push_back(Mat(5,5,DataType<float>::type));//hard-coded kernel size
 		cv::randn(W[o],cv::Scalar::all(0),cv::Scalar::all(0.1));
-
-		b.push_back(Mat(s,DataType<float>::type,Scalar::all(0.1)));//wrong dimension though!	
-		O.push_back(Mat(s,DataType<float>::type));//wrong dimension though!	
 	}
 }
 ConvLayer::~ConvLayer(){
@@ -259,7 +310,7 @@ std::vector<Mat>& ConvLayer::FF(std::vector<Mat> _I){
 	for(int i=0;i<d_i;++i){
 		for(int o=0;o<d_o;++o){
 			if(connection[i][o]){
-				cout << i << ',' <<  o << endl;
+				//cout << i << ',' <<  o << endl;
 				convolve(I[i],O[o],W[o]);
 				//cv::filter2D(I[i],O[o],I[i].depth(),W[o]);//change depth later
 				//and maybe even replace this function with something less rigid.
@@ -282,11 +333,16 @@ std::vector<Mat>& ConvLayer::BP(std::vector<Mat> _G){
 	//_G.size() == d_o
 	//G.size() == d_i
 	
-	auto iw = I[0].size().width;
-	auto ih = I[0].size().height;
+	auto iw = s.width;
+	auto ih = s.height;
+	auto ow = s.width;
+	auto oh = s.height;
+
+	//auto iw = I[0].size().width;
+	//auto ih = I[0].size().height;
 	
-	auto ow = _G[0].size().width;
-	auto oh = _G[0].size().height;
+	//auto ow = _G[0].size().width;
+	//auto oh = _G[0].size().height;
 
 	auto fwr = W[0].size().width/2; //kernel size
 	auto fhr = W[0].size().height/2;
@@ -296,9 +352,9 @@ std::vector<Mat>& ConvLayer::BP(std::vector<Mat> _G){
 	}
 
 	for(int o=0;o<d_o;++o){ //for each output channel(depth):
-		sigmoidPrime(O[o],O[o]);
-		_G[o] = _G[o].mul(O[o]); //element-wise
-		dW[o] = Mat::zeros(O[o].size(),DataType<float>::type);//initialize dW
+		//sigmoidPrime(O[o],O[o]);
+		//_G[o] = _G[o].mul(O[o]); //element-wise --> don't need since activation layer is separate now
+		dW[o] = Mat::zeros(W[o].size(),DataType<float>::type);//initialize dW
 
 		for(int i=0;i<d_i;++i){ //for each input channel
 			//G[i].setTo(Scalar(0)); //set all elements to zero
@@ -330,9 +386,25 @@ void ConvLayer::update(){
 		W[o] += dW[o];
 	}
 }
+
+void ConvLayer::setup(Size s){
+	this->s = s;
+	b.clear();
+	O.clear();
+	for(int o=0;o<d_o;++o){
+		b.push_back(Mat(s,DataType<float>::type,Scalar::all(0.1)));//wrong dimension though!	
+		O.push_back(Mat(s,DataType<float>::type));//wrong dimension though!	
+	}
+}
+
+Size ConvLayer::outputSize(){
+	return s; //same since conv=same
+}
+
 /* ** Pooling Layer ** */
 //currently only supports ave-pooling by opencv
 class PoolLayer : public Layer{
+	Size s_i;
 	Size s_p,s_s; //pooling size, stride size
 	std::vector<std::vector<std::vector<Point>>> S; //switches
 	std::vector<Mat> I;
@@ -340,9 +412,13 @@ class PoolLayer : public Layer{
 	std::vector<Mat> G;
 
 public:
-	std::vector<Mat>& FF(std::vector<Mat> I);
-	std::vector<Mat>& BP(std::vector<Mat> G);
 	PoolLayer(Size i, Size o);
+
+	virtual std::vector<Mat>& FF(std::vector<Mat> I);
+	virtual std::vector<Mat>& BP(std::vector<Mat> G);
+
+	virtual Size outputSize();
+	virtual void setup(Size);
 };
 
 PoolLayer::PoolLayer(Size s_p, Size s_s):s_p(s_p),s_s(s_s){
@@ -350,6 +426,7 @@ PoolLayer::PoolLayer(Size s_p, Size s_s):s_p(s_p),s_s(s_s){
 }
 
 std::vector<Mat>& PoolLayer::FF(std::vector<Mat> _I){
+	s_i = _I[0].size();
 	S.resize(_I.size());
 	O.resize(_I.size());
 
@@ -417,28 +494,109 @@ std::vector<Mat>& PoolLayer::BP(std::vector<Mat> _G){
 	return G;
 }
 
+void PoolLayer::setup(Size s){
+	s_i = s;
+}
+Size PoolLayer::outputSize(){
+	int w = (s_i.width-s_p.width+s_s.width-1)/s_s.width;
+	int h = (s_i.height-s_p.height+s_s.height-1)/s_s.height;
+	return Size(w,h);
+}
+
+class SoftMaxLayer: public Layer{
+private:
+	int d;
+	Size s;
+	std::vector<Mat> I;
+	std::vector<Mat> O;
+	std::vector<Mat> G;
+public:
+	SoftMaxLayer();
+
+	virtual std::vector<Mat>& FF(std::vector<Mat> I);
+	virtual std::vector<Mat>& BP(std::vector<Mat> G);
+	virtual void setup(Size s);
+	virtual Size outputSize();
+	//no need to update since to trainable parameter
+};
+
+SoftMaxLayer::SoftMaxLayer(){
+	d=0;
+}
+
+std::vector<Mat>& SoftMaxLayer::FF(std::vector<Mat> _I){
+	d = _I.size();
+	O.resize(d);
+	G.resize(d);
+	I.swap(_I);
+	for(int i=0;i<d;++i){
+		SoftMax(I[i],O[i]);
+	}
+	return O;
+}
+std::vector<Mat>& SoftMaxLayer::BP(std::vector<Mat> _G){
+	G.swap(_G);
+	return G;
+}
+void SoftMaxLayer::setup(Size s){
+	this->s=s;
+}
+Size SoftMaxLayer::outputSize(){
+	return s;
+}
 
 class ConvNet{
-	std::vector<Layer> L;
-	std::vector<Mat> FF(std::vector<Mat> X){
+private:
+	std::vector<Layer*> L;
+public:
+	
+	ConvNet(){
+		cv::theRNG().state = time(0);
+	}
+	~ConvNet(){
 		for(auto& l : L){
-			X = l.FF(X);
+			delete l;
+		}
+	}
+
+	std::vector<Mat> FF(std::vector<Mat> X){
+		//cout << "X : " << endl << X[0] << endl;
+		for(auto& l : L){
+			//cout << "X : " << endl << X[0] << endl;
+			X = l->FF(X);
 		}
 		return X;
 	}
+
 	void BP(std::vector<Mat> X, std::vector<Mat> Y){
 		//sample ..
 		auto G = FF(X);
+		//cout << "G_p : " << endl << G[0] << endl;
+		//cout << "Y[0] : " << endl << Y[0] << endl;
 		for(size_t i=0;i<Y.size();++i){
-			G[i] -= Y[i];
+			G[i] = Y[i] - G[i];
 		}
-		for(auto i = L.rbegin()+1; i != L.rend(); ++i){
-			G = i->BP(G);
+		//cout << "G_n : " << endl << G[0] << endl;
+
+		for(auto i = L.rbegin(); i != L.rend(); ++i){
+			G = (*i)->BP(G);
+			//cout << "G" << " : "<< endl << G[0].at<float>(0,0) << endl;
 		}
+
 		for(auto& l : L){
-			l.update();
+			l->update();
 		}
 	}
+	void setup(Size s){
+		for(auto& l : L){
+			l->setup(s);
+			s = l->outputSize();
+		}
+	}
+	void push_back(Layer* l){
+		L.push_back(l);
+	}
+
 };
 
 
@@ -453,7 +611,7 @@ int testConvLayer(int argc, char* argv[]){
 	imshow("M",img);
 	img.convertTo(img,CV_32F);
 	
-	auto cl = ConvLayer(1,3,img.size());
+	auto cl = ConvLayer(1,3);
 	auto al = ActivationLayer();
 
 	std::vector<Mat> I;
@@ -488,8 +646,8 @@ int testDenseLayer(int argc, char* argv[]){
 	namedWindow("M",WINDOW_AUTOSIZE);
 	imshow("M",img);
 	img.convertTo(img,CV_32F);
-	auto dl = DenseLayer(1, img.rows*img.cols, 3);
-	auto fl = FlattenLayer();
+	auto dl = DenseLayer(1, 3);
+	auto fl = FlattenLayer(1);
 
 	std::vector<Mat> I;
 	I.push_back(img);
@@ -532,8 +690,7 @@ int testPoolLayer(int argc, char* argv[]){
 	return 0;
 }
 
-
-int main(int argc, char* argv[]){
+int testLayerStack(int argc, char* argv[]){
 	//testPoolLayer(argc,argv);
 	//testDenseLayer(argc,argv);
 	
@@ -550,14 +707,14 @@ int main(int argc, char* argv[]){
 	std::vector<Mat> I;
 	I.push_back(img);
 
-	auto cl_1 = ConvLayer(1,6,img.size());
+	auto cl_1 = ConvLayer(1,6);
 	auto al_1 = ActivationLayer();
 	auto pl_1 = PoolLayer(Size(5,5),Size(3,3));
-	auto cl_2 = ConvLayer(6,16,Size(127,90));
+	auto cl_2 = ConvLayer(6,16);
 	auto al_2 = ActivationLayer();
 	auto pl_2 = PoolLayer(Size(5,5),Size(3,3));
-	auto fl = FlattenLayer(); // arbitrary... frankly don't know
-	auto dl = DenseLayer(1,19024,10);
+	auto fl = FlattenLayer(16); // arbitrary... frankly don't know
+	auto dl = DenseLayer(1,10);
 	auto al_3 = ActivationLayer();
 
 	auto m = cl_1.FF(I);
@@ -587,4 +744,75 @@ int main(int argc, char* argv[]){
 
 
 	return 0;
+
+}
+
+int testConvNet(int argc, char* argv[]){
+	if(argc != 2){
+		cout << "SPECIFY CORRECT ARGS" << endl;
+		return -1;
+	}
+	//auto img = imread(argv[1],IMREAD_ANYDEPTH);
+	//namedWindow("M",WINDOW_AUTOSIZE);
+	//imshow("M",img);
+	//img.convertTo(img,CV_32F,1/256.0);
+	Mat img = (Mat_<float>(3,3) << 1,9,4,7,2,8,3,6,5) * 0.1;
+	Mat cla = (Mat_<float>(5,1) << 0.3,0.1,0.4,0.05,0.15);
+	std::vector<Mat> X;
+	X.push_back(img);
+
+	std::vector<Mat> Y;
+	Y.push_back(cla);
+
+	ConvNet net;
+	//net.push_back(new ConvLayer(1,6)); //having to specify img.size() is annoying
+	//net.push_back(new ActivationLayer());
+	//net.push_back(new PoolLayer(Size(5,5),Size(3,3)));
+	//net.push_back(new ConvLayer(6,16)); //having to know 127,90 is annoying
+	//net.push_back(new ActivationLayer());
+	//net.push_back(new PoolLayer(Size(5,5),Size(3,3)));
+	//net.push_back(new FlattenLayer(16));
+	//net.push_back(new DenseLayer(1,10)); //having to know 19024 is annoying
+	//net.push_back(new ActivationLayer());
+	//net.push_back(new SoftMaxLayer());
+	//net.setup(img.size());
+	
+	net.push_back(new FlattenLayer(1));
+	net.push_back(new DenseLayer(1,7)); //d, s_o
+	net.push_back(new ActivationLayer());
+	net.push_back(new DenseLayer(1,5));
+	net.push_back(new ActivationLayer());
+	net.push_back(new SoftMaxLayer());
+	net.setup(img.size());
+
+	auto m = net.FF(X);
+	
+	std::cout << "M" << endl << m[0] << endl;
+	for(int i=0;i<100;++i)
+		net.BP(X,Y);
+
+	std::cout << "M" << endl << m[0] << endl;
+	m = net.FF(X);
+	std::cout << "M" << endl << m[0] << endl;
+	std::cout << "TARGET " << endl << Y[0] << endl;
+	//auto m = net.FF(I);
+	//std::cout << "M" << endl << m[0] << endl;
+
+	////hope that BP will change weights
+	//std::vector<Mat> n(1);
+	//m[0].copyTo(n[0]);
+	//parallel_for_(Range(0,n[0].rows*n[0].cols),ForEach(n[0].data,[](float a){return a - 0.1;}));
+
+	//std::cout << "N" << endl << n[0] << endl;
+	//
+	//net.BP(I,n);
+
+	//m = net.FF(I);
+	//std::cout << "M_NOW: " << endl << m[0] << endl;
+
+	return 0;
+}
+
+int main(int argc, char* argv[]){
+	return testConvNet(argc,argv);
 }
