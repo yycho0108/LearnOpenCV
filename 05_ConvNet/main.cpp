c61dcd0ba869f29f9d5c1ab0d9584636911b48f6
yycho0108 <jchocholate@gmail.com>
2016-03-21 03:50:33 -0400

    ConvNet Working with 86% Accuracy

--- 05_ConvNet/main.cpp
+++ 05_ConvNet/main.cpp
@@ -15,15 +15,15 @@ using namespace std;
 
 void setup(ConvNet& net){
 	/* ** CONV LAYER TEST ** */
-	net.push_back(new ConvolutionLayer(1,2));
+	net.push_back(new ConvolutionLayer(1,12));
 	net.push_back(new ActivationLayer("relu"));
 	net.push_back(new PoolLayer(Size(2,2),Size(2,2)));
 
-	//net.push_back(new ConvolutionLayer(12,16));
-	//net.push_back(new ActivationLayer("relu"));
-	//net.push_back(new PoolLayer(Size(2,2),Size(2,2)));
+	net.push_back(new ConvolutionLayer(12,16));
+	net.push_back(new ActivationLayer("relu"));
+	net.push_back(new PoolLayer(Size(2,2),Size(2,2)));
 		
-	net.push_back(new FlattenLayer(2));
+	net.push_back(new FlattenLayer(5));
 	net.push_back(new DenseLayer(1,84));
 	net.push_back(new ActivationLayer("sigmoid"));
 	net.push_back(new DenseLayer(1,10));
